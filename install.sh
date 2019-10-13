294cc4af9740c124be34b4fde4b9251dbd346989
Jordan Bishop <jordanbishop26@gmail.com>
2019-10-13 18:57:03 +1000

    Change copying of config files to creation of symlinks

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 # -----------------------------------------------------------------------------
 # bash
 # -----------------------------------------------------------------------------
-echo "Backing up bashrc..."
+echo "Backing up bashrc"
 cp ~/.bashrc ~/.bashrc.backup
 
-echo "Checking if customisations will be recognised by existing bashrc..."
+echo "Checking if customisations will be recognised by existing bashrc"
 add_customisations_cmd="source ~/.bashrc_customisations"
 if ! grep -q "$add_customisations_cmd" ~/.bashrc; then
-    echo "Customisations not recognised, modifying bashrc..."
-    # append command to bashrc
+    echo "Customisations not recognised, modifying bashrc"
     echo "$add_customisations_cmd" >> ~/.bashrc
 fi
 
-echo "Copying bashrc customisations..."
-cp .bashrc_customisations ~/.bashrc_customisations
-
-echo -e "\n"
+echo "Making symlink to bashrc customisations"
+ln -sf $(readlink -f .bashrc_customisations) ~/.bashrc_customisations
 
 # source new bashrc for env vars used in following nvim section
 source ~/.bashrc
@@ -23,7 +20,7 @@ source ~/.bashrc
 # -----------------------------------------------------------------------------
 # nvim
 # -----------------------------------------------------------------------------
-echo "Backing up existing nvim environment..."
+echo "Backing up existing nvim environment"
 # remove old backups
 rm -rf $VIMCONFIG.backup
 rm -rf $VIMDATA.backup
@@ -31,38 +28,34 @@ rm -rf $VIMDATA.backup
 cp -r $VIMCONFIG $VIMCONFIG.backup
 cp -r $VIMDATA $VIMDATA.backup
 
-echo "Creating clean nvim environment..."
+echo "Creating clean nvim environment"
 rm -rf $VIMCONFIG
 rm -rf $VIMDATA
 mkdir $VIMCONFIG
 mkdir $VIMDATA
 
-echo "Copying nvim config into environment..."
-cp init.vim $VIMCONFIG/init.vim
+echo "Making symlink to nvim config"
+ln -sf $(readlink -f init.vim) $VIMCONFIG/init.vim
 
-echo "Installing python packages for nvim..."
+echo "Installing python packages for nvim"
 # python3 provider
 pip3 install --upgrade pynvim
 # packages for ALE
 pip3 install --upgrade flake8 isort yapf
 
-echo "Installing minpac..."
+echo "Installing minpac"
 mkdir -p $VIMCONFIG/pack/minpac/opt
 git clone https://github.com/k-takata/minpac.git \
 	$VIMCONFIG/pack/minpac/opt/minpac
 
-echo "Installing plugins..."
+echo "Installing plugins"
 nvim --headless +PackUpdateAndQuit
-
-echo -e "\n"
 
 # -----------------------------------------------------------------------------
 # tmux
 # -----------------------------------------------------------------------------
-echo "Backing up existing tmux config..."
+echo "Backing up existing tmux config"
 cp ~/.tmux.conf ~/.tmux.conf.backup
 
-echo "Replacing existing tmux config..."
-cp .tmux.conf ~/.tmux.conf
-
-echo -e "\n"
+echo "Making symlink to tmux config"
+ln -sf $(readlink -f .tmux.conf) ~/.tmux.conf
