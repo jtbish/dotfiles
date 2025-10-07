17a33df92a9f9f22419139fe25c2927ffbdb7e44
jbish53 <jbish53@protonmail.com>
2025-10-07 14:16:10 +1000

    Update 2025: remove gitconfig, adjust install script to make less jank

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Setup
+# setup
 # -----------------------------------------------------------------------------
 # pull in customisations for bash (access to env vars and funcs)
 source .bashrc_custom
@@ -14,11 +14,21 @@ echo "Checking if customisations will be recognised by existing bashrc"
 add_customisations_cmd="source ~/.bashrc_custom"
 if ! grep -q "$add_customisations_cmd" ~/.bashrc; then
     echo "Customisations not recognised, modifying bashrc"
+    echo "# Added by jbish" >> ~/.bashrc
     echo "$add_customisations_cmd" >> ~/.bashrc
 fi
 
 echo "Making symlink to bashrc customisations"
 ln -srf .bashrc_custom ~/.bashrc_custom
+
+# -----------------------------------------------------------------------------
+# tmux
+# -----------------------------------------------------------------------------
+echo "Backing up existing tmux config"
+bu ~/.tmux.conf
+
+echo "Making symlink to tmux config"
+ln -srf .tmux.conf ~/.tmux.conf
 
 # -----------------------------------------------------------------------------
 # nvim
@@ -36,22 +46,17 @@ mkdir $VIMDATA
 echo "Making symlink to nvim config"
 ln -srf init.vim $VIMCONFIG/init.vim
 
-echo "Installing python packages for nvim"
-# python3 provider
-pip3 install --upgrade --user pynvim
-# packages for ALE
-pip3 install --upgrade --user flake8 isort yapf
-
 echo "Installing system packages for nvim"
+# nvim python 3 provider
+sudo apt install python3-neovim
+# python env management for below python packages
+sudo apt install pipx
 # shell check static analyser
-if grep -iq fedora /etc/*release; then
-    sudo dnf install ShellCheck
-elif grep -iq ubuntu /etc/*release; then
-    sudo apt install shellcheck
-else
-    echo "You aren't using Fedora or Ubuntu, so I don't know how to install
-    the shellcheck package. Sorry!"
-fi
+sudo apt install shellcheck
+
+echo "Installing python packages for nvim"
+# python3 provider and ALE packages
+pipx install pynvim flake8 isort yapf
 
 echo "Installing minpac"
 mkdir -p $VIMCONFIG/pack/minpac/opt
@@ -60,21 +65,3 @@ git clone https://github.com/k-takata/minpac.git \
 
 echo "Installing nvim plugins"
 nvim --headless +PackUpdateAndQuit
-
-# -----------------------------------------------------------------------------
-# tmux
-# -----------------------------------------------------------------------------
-echo "Backing up existing tmux config"
-bu ~/.tmux.conf
-
-echo "Making symlink to tmux config"
-ln -srf .tmux.conf ~/.tmux.conf
-
-# -----------------------------------------------------------------------------
-# gitconfig
-# -----------------------------------------------------------------------------
-echo "Backing up existing gitconfig"
-bu ~/.gitconfig
-
-echo "Making symlink to gitconfig"
-ln -srf .gitconfig ~/.gitconfig
